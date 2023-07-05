e8b4cef7f1be94dc91b6ef53052e23aedd419c74
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-06 01:31:38 +0300

    upd movie

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,4 +1,10 @@
+import { Outlet } from 'react-router-dom';
 const MovieDetails = () => {
-    return <h2>Page MovieDetails</h2>;
-  };
-  export default MovieDetails;
+  return (
+    <>
+      <h2>Page MovieDetails</h2>
+      <Outlet />
+    </>
+  );
+};
+export default MovieDetails;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,10 @@
+import { Outlet } from 'react-router-dom';
 const Movies = () => {
-  return <h2>Page Movies</h2>;
+  return (
+    <>
+      <h2>Page Movies</h2>
+      <Outlet />
+    </>
+  );
 };
 export default Movies;
