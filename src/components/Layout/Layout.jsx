5b7da1ad9e2a99b1255777d7cdde5db09f724eeb
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-06 00:37:35 +0300

    upd Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import * as s from './Layout.styled';
 
 export const Layout = () => {
--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,6 +1,7 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const StyledLink = styled.NavLink`
+export const StyledLink = styled(NavLink)`
   color: #212121;
   &.active {
     color: orangered;
