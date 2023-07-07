b6c8ce9e887fe4563aeee69e9cfc64f3cdbfcc2b
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-07 16:16:05 +0300

    upd get

--- src/api/getMoviesTrending.js
+++ src/api/getMoviesTrending.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
 const API_KEY = '0b46dbc328ca74a0ee97be896e4cf159';
 
-export const fetchMovies = page => {
+export const getMoviesTrending = page => {
   return axios('trending/movie/day', {
     params: {
       api_key: API_KEY,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,26 @@
 import { useState, useEffect } from 'react';
-// import { getMovieApi } from 'api/getMoviesApi';
 import { Section } from 'components/Section/Section';
 import { MoviesGallery } from 'components/MoviesGallery/MoviesGallery';
-import { fetchMovies } from 'api/fetchMovies';
+import { getMoviesTrending } from 'api/getMoviesTrending';
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
+  const [moviesTrending, setMoviesTrending] = useState([]);
 
   useEffect(() => {
-    fetchMovies(page)
+    getMoviesTrending(1)
       .then(({ data: { results } }) => {
-        setMovies([...results]);
+        setMoviesTrending([...results]);
       })
       .catch(err => {
         console.log('ERROR', err);
       })
       .finally(() => {});
-  }, [page]);
+  }, []);
 
   return (
     <>
       <Section title="Trending today">
-        <MoviesGallery data={movies} />
+        <MoviesGallery data={moviesTrending} />
       </Section>
     </>
   );
