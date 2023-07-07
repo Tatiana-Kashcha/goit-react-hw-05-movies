05cde4dd84effb770d4b62d8b24c460345f960d9
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-07 17:39:53 +0300

    add loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,41 @@ import { useState, useEffect } from 'react';
 import { Section } from 'components/Section/Section';
 import { MoviesGallery } from 'components/MoviesGallery/MoviesGallery';
 import { getMoviesTrending } from 'api/getMoviesTrending';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const Home = () => {
   const [moviesTrending, setMoviesTrending] = useState([]);
   const [MoviesPerPage, setMoviesPerPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
+
     getMoviesTrending(1)
       .then(({ data: { results } }) => {
         setMoviesTrending([...results]);
         setMoviesPerPage(results.length);
+
+        if (!results.length) {
+          Notify.failure(
+            'Sorry, there are no popular movies for today. Please try again.'
+          );
+        }
       })
       .catch(err => {
         console.log('ERROR', err);
       })
-      .finally(() => {});
+      .finally(() => {
+        setIsLoading(false);
+        Loading.remove();
+      });
   }, []);
 
   return (
     <>
       <Section title="Trending today">
+        {isLoading && Loading.arrows()}
         {MoviesPerPage > 0 && <MoviesGallery data={moviesTrending} />}
       </Section>
     </>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,26 +4,39 @@ import { Section } from 'components/Section/Section';
 import Searchbar from 'components/Searchbar/Searchbar';
 import { MoviesGallery } from 'components/MoviesGallery/MoviesGallery';
 import { searchMovies } from 'api/searchMovies';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const Movies = () => {
   const [searchText, setSearchText] = useState('');
   const [dataMovies, setDataMovies] = useState([]);
   const [searchMoviesPerPage, setSearchMoviesPerPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchText) {
       return;
     }
+    setIsLoading(true);
 
     searchMovies(searchText)
       .then(({ data: { results } }) => {
         setDataMovies([...results]);
         setSearchMoviesPerPage(results.length);
+
+        if (!results.length) {
+          Notify.failure(
+            'Sorry, there are no movies matching your search query.'
+          );
+        }
       })
       .catch(err => {
         console.log('ERROR', err);
       })
-      .finally(() => {});
+      .finally(() => {
+        setIsLoading(false);
+        Loading.remove();
+      });
   }, [searchText]);
 
   const handleSearch = searchText => {
@@ -33,6 +46,7 @@ const Movies = () => {
   return (
     <>
       <Section>
+        {isLoading && Loading.arrows()}
         <Searchbar onSubmit={handleSearch} />
         {searchMoviesPerPage > 0 && <MoviesGallery data={dataMovies} />}
       </Section>
