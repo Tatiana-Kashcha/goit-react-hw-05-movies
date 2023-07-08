f9ee8a21cd6ac10925edac953cafa739646c3d4c
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-08 16:39:01 +0300

    add button

--- src/components/RenderDetails/RenderDetails.jsx
+++ src/components/RenderDetails/RenderDetails.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
 import * as s from './RenderDetails.styled';
+import { ReactComponent as IconArrow } from '../icons/arrow-back.svg';
 
 export const RenderDetails = data => {
   return (
     <>
-      <s.Div>
+      <s.ButtonBack type="button">
+        <IconArrow width="15" heigth="25" />
+        Go back
+      </s.ButtonBack>
+      <s.DivDet>БУДЕ ІНФА ПРО ФІЛЬМ</s.DivDet>
+      <s.DivAdd>
         <s.Description>Additional information</s.Description>
         <s.List>
           <li>
@@ -14,7 +20,7 @@ export const RenderDetails = data => {
             <Link>Reviews</Link>
           </li>
         </s.List>
-      </s.Div>
+      </s.DivAdd>
     </>
   );
 };
--- src/components/RenderDetails/RenderDetails.styled.js
+++ src/components/RenderDetails/RenderDetails.styled.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 
-export const List = styled.ul`
-  margin-left: 40px;
-  li {
-    &:not(:last-child) {
-      margin-bottom: 10px;
-    }
-  }
+export const ButtonBack = styled.button`
+  width: 80px;
+  padding: 2px;
+  margin-top: 5px;
+  margin-bottom: 3px;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid rgb(202 199 199 / 67%);
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
-export const Div = styled.div`
+export const DivDet = styled.div`
+  padding-bottom: 10px;
+  box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.3);
+`;
+
+export const DivAdd = styled.div`
   padding-top: 25px;
   padding-bottom: 20px;
   box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.3);
@@ -17,4 +27,13 @@ export const Div = styled.div`
 
 export const Description = styled.p`
   margin-bottom: 20px;
+`;
+
+export const List = styled.ul`
+  margin-left: 40px;
+  li {
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+  }
 `;
