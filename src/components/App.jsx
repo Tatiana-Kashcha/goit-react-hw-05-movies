4b3e191193868f017a7c447b1e915b395ea98569
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-10 12:16:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import { Layout } from './Layout/Layout';
 
-const Home = lazy(() => import('pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
 
